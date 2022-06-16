1bd37038a113e5113c650c869d7d7a87f7d0b3df
Amogh Dixit <amoghdixit104@gmail.com>
2022-06-16 15:55:25 +0530

    feat: connect to metamask wallet

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -17,7 +17,6 @@ const Header = () => {
 const HeaderWrapper = styled.div`
   width: 100%;
   height: 70px;
-  border: 2px solid red;
   display: flex;
   justify-content: space-between;
   align-items: center;
